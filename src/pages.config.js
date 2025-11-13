bac63835f39b9041d8c0db7bb3dc3f90a561e41a
sam <sam@efintax.biz>
2025-11-13 05:36:01 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -3,10 +3,10 @@ import Products from './pages/Products';
 import AddProduct from './pages/AddProduct';
 import Vehicles from './pages/Vehicles';
 import Parts from './pages/Parts';
-import Sales from './pages/Sales';
 import Repairs from './pages/Repairs';
 import Exports from './pages/Exports';
 import Freight from './pages/Freight';
+import Sales from './pages/Sales';
 import Layout from './Layout.jsx';
 
 
@@ -16,10 +16,10 @@ export const PAGES = {
     "AddProduct": AddProduct,
     "Vehicles": Vehicles,
     "Parts": Parts,
-    "Sales": Sales,
     "Repairs": Repairs,
     "Exports": Exports,
     "Freight": Freight,
+    "Sales": Sales,
 }
 
 export const pagesConfig = {
