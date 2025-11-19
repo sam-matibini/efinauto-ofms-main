73bd09e90e5012a29d8f0b6ab034ed021c3fa206
sam <sam@efintax.biz>
2025-11-19 19:38:59 +0000

    File changes

--- src/components/accounting/ImportTransactionsDialog.jsx
+++ src/components/accounting/ImportTransactionsDialog.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { base44 } from "@/api/base44Client";
-import { useMutation, useQueryClient } from "@antml:react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
