9f1778825ca63d26116c81ecdaccb6f65e82d81a
sam <sam@efintax.biz>
2025-11-21 06:33:55 +0000

    File changes

--- src/components/accounting/AccountDialog.jsx
+++ src/components/accounting/AccountDialog.jsx
@@ -46,37 +46,20 @@ export default function AccountDialog({ open, onClose, account }) {
 
   const saveMutation = useMutation({
     mutationFn: async (data) => {
-      try {
-        console.log("Starting mutation with data:", data);
-        console.log("Company ID:", selectedCompanyId);
-        console.log("Is update?", !!account);
-        
-        let result;
-        if (account) {
-          console.log("Updating account ID:", account.id);
-          result = await base44.entities.Account.update(account.id, data);
-        } else {
-          const createData = { ...data, company_id: selectedCompanyId };
-          console.log("Creating account with data:", createData);
-          result = await base44.entities.Account.create(createData);
-        }
-        
-        console.log("Mutation result:", result);
-        return result;
-      } catch (err) {
-        console.error("Mutation error details:", err);
-        throw err;
+      if (account) {
+        return await base44.entities.Account.update(account.id, data);
+      } else {
+        return await base44.entities.Account.create({ ...data, company_id: selectedCompanyId });
       }
     },
-    onSuccess: (data) => {
-      console.log("Save successful, invalidating queries...");
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['accounts'] });
       queryClient.invalidateQueries({ queryKey: ['accounts', selectedCompanyId] });
       toast.success(account ? "Account updated!" : "Account created!");
       onClose();
     },
     onError: (error) => {
-      console.error("Save mutation failed:", error);
-      toast.error("Failed to save: " + (error.message || JSON.stringify(error)));
+      toast.error("Failed to save account. Please try again.");
     }
   });
 
